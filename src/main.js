d05eeedf8b945a52f46eb988c7e3fae0d27dffa3
tomschmosby <57222062+tomschmosby@users.noreply.github.com>
2020-07-31 21:14:33 +0200

    change fuenf

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ Vue.config.productionTip = false
 
 Vue.use(new VueSocketIO({
   debug: true,
-  connection: 'https://hal.hfg.design:10000ampel', //WICHTIG: Hier nach dem Slash den Namen eures Bots angeben (wie auf dem Pi festgelegt)
+  connection: 'https://hal.hfg.design:10000/ampel', //WICHTIG: Hier nach dem Slash den Namen eures Bots angeben (wie auf dem Pi festgelegt)
   options: {
     useConnectionNamespace: true
   }
